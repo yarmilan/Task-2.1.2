2ba8c70fc806a5a9a2bfee1cb7234c139be70e4b
yargo <yargomich@gmail.com>
2022-12-27 18:26:57 +0300

    Task-2.1.2

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -8,5 +8,20 @@ public class App {
         HelloWorld bean =
                 (HelloWorld) applicationContext.getBean("helloworld");
         System.out.println(bean.getMessage());
+
+        HelloWorld bean1 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean1.getMessage());
+        System.out.println(bean == bean1);
+
+        Cat catBean =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+
+        Cat catBean1 =
+                (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean1.getMessage());
+        System.out.println(catBean == catBean1);
+
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -10,5 +10,14 @@ public class AppConfig {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+
+    }
+
+    @Bean(name = "cat")
+    @Scope("prototype")
+    public Cat getCat () {
+        Cat cat = new Cat();
+        cat.setMessage("Cat");
+        return cat;
     }
 }
